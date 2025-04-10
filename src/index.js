264229be9e8b7d3329f2df1ff369d1175fa413b0
school <yho45@my.centennialcollege.ca>
2025-04-10 00:21:23 -0400

    deploy fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));  // Updated for React 18+
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+    document.getElementById('root'),
+  )
