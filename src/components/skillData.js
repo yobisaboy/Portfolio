d24148ddd785da29f8b7ff2e9caa2a1433c85483
school <yho45@my.centennialcollege.ca>
2025-04-11 17:31:27 -0400

    profile: added skill section

---
+++ src/components/skillData.js
@@ -0,0 +1,37 @@
+// skillData.js
+
+const skillData = [
+    {
+      category: 'Game Development',
+      skills: [
+        'Unity 3D',
+        'Unity XR',
+        'Unreal Engine',
+      ],
+    },
+    {
+      category: 'Mobile Development',
+      skills: ['Android'],
+    },
+    {
+      category: 'Programming Languages',
+      skills: ['C#', 'C++', 'Java Programming', 'JavaScript', 'Kotlin'],
+    },
+    {
+      category: 'Web Development',
+      skills: [
+        'Express',
+        'GraphQL',
+        'Node.js',
+        'React',
+        'REST',
+      ],
+    },
+    {
+      category: 'Databases',
+      skills: ['MongoDB (NoSQL)', 'MySQL (SQL)'],
+    },
+  ];
+  
+  export default skillData;
+  
